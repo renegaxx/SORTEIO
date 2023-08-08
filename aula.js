a6006f287c011a34fe90feccd5f443c2cd6833a7
renegaxx <133661895+renegaxx@users.noreply.github.com>
2023-08-08 08:21:54 -0300

    Initial commit

---
+++ aula.js
@@ -0,0 +1,15 @@
+console.log(Math.random9());
+
+console.log(Match.random() *20);
+
+console.log(Math.round(3.1));
+console.log(Math.round(3.9));
+
+console.log(Math.cell(3.9));
+console.log(Math.cell(3.9));
+
+console.log(Math.floor(3.1));
+console.log(Math.floor(3.9));
+
+
+
